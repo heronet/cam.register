057e3f9bf197e4c548b961db66da8b57bfb4ef6e
heronet <sirat4757@gmail.com>
2025-03-16 01:11:59 +0600

    add: implement user authentication handling and define UserData model

---
+++ src/hooks.server.ts
@@ -0,0 +1,28 @@
+import type { UserData } from '$lib/models/UserData';
+import { redirect, type Handle } from '@sveltejs/kit';
+
+export const handle: Handle = async ({ event, resolve }) => {
+	const user = event.cookies.get('user');
+	if (user) event.locals.user = JSON.parse(user);
+
+	// TODO: Remove after backend integration
+	const dummyUser: UserData = {
+		expiry: '',
+		token: 'token',
+		user: {
+			username: 'Sirat'
+		}
+	};
+	event.locals.user = dummyUser;
+
+	if (!user) {
+		let protectedRoute = event.url.pathname.startsWith('/admin');
+
+		if (protectedRoute) {
+			throw redirect(303, '/');
+		}
+	}
+
+	const response = await resolve(event);
+	return response;
+};
---
+++ src/lib/models/UserData.ts
@@ -0,0 +1,9 @@
+export interface UserData {
+	expiry: string;
+	token: string;
+	user: User;
+}
+
+export interface User {
+	username: string;
+}
